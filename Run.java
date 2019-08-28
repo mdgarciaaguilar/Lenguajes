748193d071aa0632ad4cc0a21614755569805d38
Lourdes Navarrete Rodríguez <lulynavarrete@hotmail.com>
2019-08-28 14:22:13 -0500

    Create Run.java

---
+++ Run.java
@@ -0,0 +1,25 @@
+
+import Scanner.Scanner;
+import Scanner.Token;
+import java.util.List;
+
+public class Run {
+
+    public static void main(String[] args) {
+
+        String string;
+        Scanner scanner;
+        scanner = new Scanner();
+        List<Token> tokens;        
+        string = "101b-(001b     +11b)";
+        //string = "101b+11b";
+        //string = "101b(+11b)";
+        //string = "-+(-101b(+11b)"; 
+        System.out.println(string);
+        tokens = scanner.scan(string);
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+}
